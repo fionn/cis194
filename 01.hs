08b5078653b9e73d4694119502a1a8474ecee1a9
Fionn Fitzmaurice <iamfionn@github.com>
2018-10-28 23:14:38 +0800

    solve the 4-peg tower of hanoi

--- 01.hs
+++ 01.hs
@@ -41,11 +41,31 @@ hanoi n base target temp
                   ++ hanoi 1 base target temp
                   ++ hanoi (n - 1) temp target base
 
+hanoi4 :: Int -> Peg -> Peg -> Peg -> Peg -> [Move]
+hanoi4 n a b c d
+    | n < 2     = hanoi n a b c
+    | otherwise = hanoi4 (n - k) a d b c
+                  ++ hanoi k a b c
+                  ++ hanoi4 (n - k) d b a c
+      where k = optimalPartition n
+
+quadraticRoot :: Float -> Float -> Float -> Float
+quadraticRoot a b c = (-b + sqrt (b * b - 4 * a * c)) / (2 * a)
+
+optimalPartition :: Int -> Int
+optimalPartition = floor . quadraticRoot 1 1 . fromIntegral . negate . (* 2)
+
 -- main
 
 main :: IO ()
 main = do
     putStrLn "Luhn algorithm:"
     print $ validate 4012888888881881
-    putStrLn "Towers of Hanoi (3 pegs, 4 discs):"
+    putStrLn "Towers of Hanoi, 3 pegs, 4 discs:"
     print $ hanoi 4 "a" "b" "c"
+    putStrLn "Takes 32767 moves for 15 pegs:"
+    print $ 32767 == length (hanoi 15 "a" "b" "c")
+    putStrLn "Towers of Hanoi, 4 pegs, 4 discs:"
+    print $ hanoi4 4 "a" "b" "c" "d"
+    putStrLn "Takes 129 moves for 15 pegs:"
+    print $ 129 == length (hanoi4 15 "a" "b" "c" "d")
